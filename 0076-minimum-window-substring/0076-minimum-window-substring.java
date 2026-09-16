4c99cc30b6c23478221d9ea06f65d758c87fd392
Afzal Nizami <afzal92239@gmail.com>
2026-09-16 12:53:39 +0530

    Time: 15 ms (57.31%), Space: 47.2 MB (42.44%) - LeetHub

--- 0076-minimum-window-substring/0076-minimum-window-substring.java
+++ 0076-minimum-window-substring/0076-minimum-window-substring.java
@@ -1,30 +1,29 @@
 class Solution {
     public String minWindow(String s, String t) {
         HashMap<Character,Integer> map=new HashMap<>();
-        char[] arr=t.toCharArray();
-        char[] nums=s.toCharArray();
-        for(char ele:arr){
-            map.put(ele,map.getOrDefault(ele,0)+1);
+        for(int i=0;i<t.length();i++){
+            char ch=t.charAt(i);
+            map.put(ch,map.getOrDefault(ch,0)+1);
         }
-        int i=0,j=0,max=Integer.MAX_VALUE,start=0,count=arr.length;
-        while(j<nums.length){
-            if(map.containsKey(nums[j])){
-                map.put(nums[j],map.get(nums[j])-1);
-                if(map.get(nums[j])>=0) count--;
+        int i=0,max=Integer.MAX_VALUE,count=t.length(),start=0;
+        for(int j=0;j<s.length();j++){
+            char ch=s.charAt(j);
+            if(map.containsKey(ch)){
+                map.put(ch,map.get(ch)-1);
+                if(map.get(ch)>=0) count--;
             }
             while(count==0){
                 if(max>j-i+1){
                     max=j-i+1;
                     start=i;
                 }
-                char left=nums[i];
-                if(map.containsKey(left)){
-                    map.put(left,map.get(left)+1);
-                    if(map.get(left)>0) count++;
+                char c=s.charAt(i);
+                if(map.containsKey(c)){
+                    map.put(c,map.get(c)+1);
+                    if(map.get(c)>0) count++;
                 }
                 i++;
             }
-            j++;
         }
         if(max==Integer.MAX_VALUE) return "";
         return s.substring(start,start+max);
