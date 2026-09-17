01f858569891c09e0a8fc5c894be86dc787234c7
Afzal Nizami <afzal92239@gmail.com>
2026-09-17 13:29:34 +0530

    Time: 2 ms (86.7%), Space: 49.2 MB (71.68%) - LeetHub

--- 0143-reorder-list/0143-reorder-list.java
+++ 0143-reorder-list/0143-reorder-list.java
@@ -9,28 +9,44 @@
  * }
  */
 class Solution {
+    public ListNode reverse(ListNode head){
+        ListNode curr=head;
+        ListNode fwd=null;
+        ListNode prev=null;
+        while(curr!=null){
+            fwd=curr.next;
+            curr.next=prev;
+            prev=curr;
+            curr=fwd;
+        }
+        return prev;
+    }
     public void reorderList(ListNode head) {
-        ArrayList<ListNode> arr=new ArrayList<>();
-        ListNode temp=head;
-        while(temp!=null){
-            arr.add(temp);
-            temp=temp.next;
+        ListNode slow=head;
+        ListNode fast=head;
+        while(fast!=null && fast.next!=null){
+            slow =slow.next;
+            fast=fast.next.next;
         }
-        ArrayList<ListNode> ar=new ArrayList<>();
-        int i=0;
-        int j=arr.size()-1;
-        while(arr.size()-ar.size()>0){
-            ar.add(arr.get(i));
-            i++;
-            ar.add(arr.get(j));
-            j--;
-        }
+        ListNode a=slow.next;
+        a=reverse(a);
+        slow.next=null;
+        ListNode t1=head;
+        ListNode t2=a;
         ListNode dummy=new ListNode(-1);
-        ListNode t=dummy;
-        for(ListNode k:ar){
-            t.next=k;
-            t=t.next;
+        ListNode tempo=dummy;
+        while(t1!=null && t2!=null){
+            tempo.next=t1;
+            t1=t1.next;
+            tempo=tempo.next;
+            tempo.next=t2;
+             t2=t2.next;
+            tempo=tempo.next;
         }
-        t.next=null;
+        if(t1==null){
+            tempo.next=t2;
+        }else{
+            tempo.next=t1;
+        }
     }
 }
