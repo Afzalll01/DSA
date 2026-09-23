340659c5a5af5ae061a791335f17608cb2a3459f
Afzal Nizami <afzal92239@gmail.com>
2026-09-24 01:06:19 +0530

    Time: 6 ms (43.81%), Space: 46.7 MB (10.11%) - LeetHub

---
+++ 0137-single-number-ii/0137-single-number-ii.java
@@ -0,0 +1,12 @@
+class Solution {
+    public int singleNumber(int[] nums) {
+        HashMap<Integer,Integer> map=new HashMap<>();
+        for(int ele:nums){
+            map.put(ele,map.getOrDefault(ele,0)+1);
+        }
+        for(int ele:nums){
+            if(map.get(ele)==1) return ele;
+        }
+        return 0;
+    }
+}
